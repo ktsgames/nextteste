bf32845f9064c209e297c781ddf1f77cf3e76430
Kaique Tavares <162934069+ktsgames@users.noreply.github.com>
2025-01-01 20:33:19 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 // app/page.js
+import Head  from 'next/head';
 import Inicio from '@/app/Componentes/inicio/Inicio'
 
 export default function Home() {
   return (
     
      <div>
+           <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Meu Site</title>
+      </Head>
         <Inicio/>
         
      </div>
