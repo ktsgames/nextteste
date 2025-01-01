d9732706465ca2dacf85104cdd8557190f8b1b55
Kaique Tavares <162934069+ktsgames@users.noreply.github.com>
2025-01-01 20:13:25 -0300

    deleta desnecessarios
    apaguei importações desnecessarias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 // app/page.js
-import Barra from '@/app/Componentes/barra-menu.jsx/barra-menu'
-import { Button } from 'react-bootstrap';  // Importando o componente Button do React Bootstrap
 import Inicio from '@/app/Componentes/inicio/Inicio'
 
 export default function Home() {
